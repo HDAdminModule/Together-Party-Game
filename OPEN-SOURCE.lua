c38cdc5e716ad7c7e80faf756a8dbb4e79a19d6a
IncognitoMode52’s funny github <fatimashoma44@gmail.com>
2025-02-06 22:27:26 -0500

    Update OPEN-SOURCE.lua

--- OPEN-SOURCE.lua
+++ OPEN-SOURCE.lua
@@ -1,38 +1,40 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local localchar = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
 local Window = Rayfield:CreateWindow({
-   Name = "Together",
-   Icon = 0,
-   LoadingTitle = "git rayfield'd",
-   LoadingSubtitle = "nil",
-   Theme = "Default",
+   Name = "Together [Party Game] ",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "lulz",
+   LoadingSubtitle = "cgi",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
    ConfigurationSaving = {
       Enabled = false,
-      FolderName = nil,
-      FileName = "idk"
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
    },
 
    Discord = {
-      Enabled = false,
-      Invite = "noinvitelink",
-      RememberJoins = true
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
 
-   KeySystem = false,
+   KeySystem = false, -- Set this to true to use our key system
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key",
-      SaveKey = true,
-      GrabKeyFromSite = false,
-      Key = {"Hello"}
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
-  local Tab1 = Window:CreateTab("Teleport", 9943307899)
+})
+
+local Tab1 = Window:CreateTab("Teleport", 9943307899)
   local Button = Tab1:CreateButton({
    Name = "TP to Door",
    Callback = function()
@@ -65,5 +67,4 @@ local Window = Rayfield:CreateWindow({
            end
        end
    end,
-})
 })
